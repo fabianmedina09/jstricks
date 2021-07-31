8905c872ed1af394a87fd1ae11d8d0293f53d0dc
fabianmedina09 <fabianmedina09@gmail.com>
2021-07-31 15:43:26 -0500

    Vídeo 1

--- src/Video1.ts
+++ src/Video1.ts
@@ -1,14 +1,25 @@
 console.log('Vídeo 1');
 
-// 1. Sumar elementos de un arreglo
+const title1 = 'Sumar elementos de un arreglo';
 const numbers = [6, 8, 2, 4];
 
 // JS
 const sumJS = numbers.reduce((x, y) => x + y);
-console.log(sumJS); // returns 20
 
 // TS
 const sumTS: number = numbers.reduce(
   (previus: number, current: number) => previus + current
 );
-console.log(sumTS); // returns 20
+console.log(title1, sumJS, sumTS);
+// returns 20
+
+// #################################################
+
+const title2 = 'Acortar condicionales';
+const title3 = 'Validando asignaciones';
+const title4 = 'Operador (,)';
+const title5 = '(length) para recortar arreglos';
+const title6 = 'Destructuring y reasignación con ([]';
+const title7 = 'Ordenar arreglo aleatoriamente';
+const title8 = 'Nombre de objetos dinámicos';
+const title9 = 'Eliminar duplicados';
