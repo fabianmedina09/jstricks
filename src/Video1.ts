5d87b8fc78b9fda63f1ac855128d82145897eddf
fabianmedina09 <fabianmedina09@gmail.com>
2021-07-31 15:25:33 -0500

    Suma

--- src/Video1.ts
+++ src/Video1.ts
@@ -1,1 +1,14 @@
 console.log('Vídeo 1');
+
+// 1. Sumar elementos de un arreglo
+const numbers = [6, 8, 2, 4];
+
+// JS
+const sumJS = numbers.reduce((x, y) => x + y);
+console.log(sumJS); // returns 20
+
+// TS
+const sumTS: number = numbers.reduce(
+  (previus: number, current: number) => previus + current
+);
+console.log(sumTS); // returns 20
